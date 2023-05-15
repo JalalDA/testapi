450fa8a8e60a44165eccc042e76ef8476ebd9046
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-05-15 19:24:38 +0700

    buang db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require("express")
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 const app = express()
 
@@ -12,9 +12,9 @@ app.listen(port, ()=>{
     console.log(`App listen on port ${port}`);
 })
 
-mongoose.connect(mongourl).then(()=>{
-    console.log("DB Connected");
-}).catch((err)=>console.log("Error connected"))
+// mongoose.connect(mongourl).then(()=>{
+//     console.log("DB Connected");
+// }).catch((err)=>console.log("Error connected"))
 
 app.get('/', (req, res)=>{
     res.status(200).json({
