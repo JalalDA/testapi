53f24392bc22b8162dc0a27c52da325afdf0fffb
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-05-15 19:13:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require("express")
+const mongoose = require('mongoose')
+
+const app = express()
+
+const mongourl = process.env.MONGO_URL
+const port = process.env.PORT
+
+app.listen(port, ()=>{
+    console.log(`App listen on port ${port}`);
+})
+
+mongoose.connect(mongourl).then(()=>{
+    console.log("DB Connected");
+}).catch((err)=>console.log("Error connected"))
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        msg : "Wellcome to my test API"
+    })
+})
