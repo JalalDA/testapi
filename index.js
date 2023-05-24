70197f67309d3c884f6d22e85371b1ab81f35e4d
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-05-24 23:46:56 +0700

    add socket

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require("express")
+const http = require('http')
+const {Server} = require("socket.io")
 // const mongoose = require('mongoose')
 
 const app = express()
 
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors :{
+        origin : "*"
+    }
+})
+
 const mongourl = process.env.MONGO_URL
 const port = process.env.PORT
 
-app.listen(port, ()=>{
+io.on('connection', socket=>{
+    socket.on("login", user=>{
+        `${user} has login with socketId = ${socket.id}`
+    })
+})
+
+server.listen(port, ()=>{
     console.log(`App listen on port ${port}`);
 })
 
